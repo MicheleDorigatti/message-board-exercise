bc6ba67ede3dfd5ee6a537fa317e79e29e331a73
Michele Dorigatti <michele0dorigatti@gmail.com>
2019-11-12 18:16:57 +0100

    Add server, some API and tests

--- src/main/java/board/MainVerticle.java
+++ src/main/java/board/MainVerticle.java
@@ -1,18 +1,91 @@
 package board;
 
+import io.netty.util.concurrent.Promise;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainVerticle extends AbstractVerticle {
+    HttpServer server;
 
   @Override
   public void start(Future<Void> startFuture) {
+    Router router = Router.router(vertx);
+    router.route().handler(BodyHandler.create());
+    setRoutes(router);
+    server = vertx
+            .createHttpServer()
+            .requestHandler(router)
+            .listen(8080, result -> {
+              if (result.succeeded()) {
+                System.out.println("Board server started");
+                startFuture.complete();
+              } else {
+                startFuture.fail(result.cause());
+              }
+            });
   }
 
-  private void setRoutes(Router router){
-  }
+    @Override
+    public void stop(Future<Void> stopFuture) throws Exception {
+        server.close();
+        stop();
+        stopFuture.complete();
+    }
 
+  private void setRoutes(Router router) {
+      // Get to root
+      router.get("/").handler(req -> {
+          int client = 0;
+          JsonObject json = new JsonObject()
+                  .put("client", client)
+                  .put("links", new JsonArray()
+                          .add(new JsonObject()
+                                  .put("href", "/board")
+                                  .put("rel", "list")
+                                  .put("method", "GET"))
+                          .add(new JsonObject()
+                                  .put("href", "/board/" + client)
+                                  .put("rel", "create")
+                                  .put("method", "POST")));
+        req.response()
+                .putHeader("content-type", "application/json")
+                .end(json.encode());
+      });
+
+      // A client can view all messages in the service
+      router.get("/board").handler(req -> {
+          JsonObject json = new JsonObject()
+                  .put("messages", new JsonArray()); // TODO: read the messages
+          req.response()
+                  .putHeader("content-type", "application/json")
+                  .end(json.encode());
+      });
+
+      // A client can create a message in the service
+      router.post("/board/:client").handler(req -> {
+          int client = Integer.parseInt(req.request().getParam("client"));
+          JsonObject req_json = req.getBodyAsJson();
+          System.out.println(req_json.encodePrettily());
+          String text = req_json.getString("text");
+
+          req.response()
+                  .putHeader("content-type", "application/json")
+                  .end(req_json.encode());
+      });
+
+      // A client can modify their own messages
+      // A client can delete their own messages
+
+  }
 }
 
 
--- src/test/java/board/TestMainVerticle.java
+++ src/test/java/board/TestMainVerticle.java
@@ -1,6 +1,9 @@
 package board;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClient;
 import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
@@ -17,16 +20,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestMainVerticle {
     private MainVerticle verticle;
 
-  @BeforeEach
-  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-      verticle = new MainVerticle();
-    vertx.deployVerticle(verticle, testContext.succeeding(id -> testContext.completeNow()));
+  @Test
+  @DisplayName("Get to root")
+  @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
+  void get_root(Vertx vertx, VertxTestContext testContext) {
+      WebClient client = WebClient.create(vertx);
+      client.get(8080, "::1", "/")
+              .putHeader("Accept", "application/json")
+              .send(response -> testContext.verify(() -> {
+                  assertEquals(200, response.result().statusCode());
+                  assertEquals(response.result().bodyAsJsonObject().size(), 2);
+                  System.out.println("body\n" + response.result().bodyAsString());
+                  testContext.completeNow();
+              }));
+      client.close();
   }
 
-  @Test
-  @DisplayName("")
-  @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
-  void test(Vertx vertx, VertxTestContext testContext) {
-  }
+    @Test
+    @DisplayName("Create a message")
+    @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
+    void create_message(Vertx vertx, VertxTestContext testContext) {
+        WebClient client = WebClient.create(vertx);
+        JsonObject req_json = new JsonObject()
+                .put("client", "0")
+                .put("text", "A test message");
+        client.post(8080, "::1", "/board/0")
+                .putHeader("Accept", "application/json")
+                .sendJsonObject(req_json, response -> testContext.verify(() -> {
+                    assertEquals(200, response.result().statusCode());
+                    assertEquals(response.result().bodyAsJsonObject().size(), 2);
+                    System.out.println("body\n" + response.result().bodyAsString());
+                    testContext.completeNow();
+                }));
+        client.close();
+    }
 
 }
